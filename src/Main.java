dcb0929a41d5ce7eb9cdce5554fed4323575e102
grifo <grifoncheg1996@gmail.com>
2022-10-27 13:22:56 +0300

    дз, кроме 4 задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-
+// Задание 1-3.
+        int [] numbers = new int[3];
+        for (int i = 0; i <= numbers.length ; i++) {
+            System.out.println(numbers);
+        }
+        double [] decimal = {1.57, 7.654, 9.986} ;
+        for (int i = 0; i < decimal.length; i++) {
+            System.out.println(decimal);
+        }
+        int [] random = {9, 802, 8239283, -92, 777} ;
+        for (int i = 0; i <= random.length ; i++) {
+            System.out.println(random);
+        } // 4 задание не удалось
     }
+
+
+
+
+
+
+
+
+
+    
+    
+
+
 }
